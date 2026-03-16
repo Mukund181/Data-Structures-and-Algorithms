6df68bbf9e8de1ed0a198e838ba10f8c1d50db91
Mukund181 <mukund.fegade@gmail.com>
2026-03-16 15:47:40 +0530

    Refactor secondmax function and add optimization comment
    The function secondmax finds the second largest element in an array. An optimized version is commented out for future reference.

--- Array and strings/SecondMax.cpp
+++ Array and strings/SecondMax.cpp
@@ -44,6 +44,23 @@ int secondmax(int arr[],int n){
             return -1;
 
         return ans;
+    /* 
+        Optimized program - ( with arr as vector)
+        int largest = -1, secondLargest = -1;
+
+        for(int i = 0; i < arr.size(); i++){
+            if(arr[i] > largest){
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if(arr[i] < largest && arr[i] > secondLargest){
+                secondLargest = arr[i];
+            }
+        }
+
+        return secondLargest;
+   */
+
 }
 
 int main(){
