758293f27ebc3d36dcf92f84497ad20f5291724e
Mukund181 <mukund.fegade@gmail.com>
2026-03-16 15:33:24 +0530

    update second max program

--- Array and strings/SecondMax.cpp
+++ Array and strings/SecondMax.cpp
@@ -3,37 +3,47 @@ using namespace std;
 
 int secondmax(int arr[],int n){
     int max = arr[0];
-    int j=0;
-    for(int i=0;i<n;i++){
-        if(arr[i]>max){
-            max= arr[i];
-            j = i;
+        int j = 0;
+
+        for(int i = 0; i < n; i++){
+            if(arr[i] > max){
+                max = arr[i];
+                j = i;
+            }
         }
-    }
-    int s1= arr[j+1];
-    int s2;
-    for(int i=j+1;i<n;i++){
-        if(arr[i]>s1){
-            s1=arr[i];
+
+        int s1 = -1, s2 = -1;
+
+        // right side
+        if(j != n-1){
+            s2 = arr[j+1];
+            for(int i = j+1; i < n; i++){
+                if(arr[i] > s2 && arr[i] != max){
+                    s2 = arr[i];
+                }
+            }
         }
-    }
-    if(j==0){
-        return s1;
-    }
-    else {
-        s2 = arr[j-1];
-    }
-    for(int i=j-1;i>0;i++){
-        if(arr[i]>s2){
-            s2= arr[i];
+
+        // left side
+        if(j != 0){
+            s1 = arr[j-1];
+            for(int i = j-1; i >= 0; i--){
+                if(arr[i] > s1 && arr[i] != max){
+                    s1 = arr[i];
+                }
+            }
         }
-    }
-    if(s1>s2){
-        return s1;
-    }
-    else {
-        return s2;
-    }
+
+        int ans;
+        if(s1 > s2)
+            ans = s1;
+        else
+            ans = s2;
+
+        if(ans == -1)
+            return -1;
+
+        return ans;
 }
 
 int main(){
